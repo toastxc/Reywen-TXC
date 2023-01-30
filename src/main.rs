9f73d83b19f3429b3d3441802521318a61336523
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-30 15:22:17 +0800

    disable depricated plugins

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,102 @@
-// external packages
+// external crates
 use futures_util::StreamExt;
+use reywen::{
+    client::Do,
+    structs::{auth::Auth, message::Message},
+    websocket::Websocket,
+};
+
+// imported from reywen library
+
 // plugins
 pub mod plugins {
     pub mod bridge;
-    pub mod e6;
-    pub mod message;
-    pub mod plural;
-    pub mod shell;
-    pub mod tomo;
+    /*
+        pub mod e6;
+        pub mod message;
+        pub mod plural;
+        pub mod shell;
+        pub mod tomo;
+    */
 }
-
-// reywen lib
-use reywen::{
-    bonfire::bonfire::from_ws,
-    delta::{
-        delta::rev_message_in,
-        fs::{conf_init, ws_init},
-        oop::Reywen,
-    },
-};
-
-use crate::plugins::{
-    bridge::br_main, e6::e6_main, message::message_main, plural::plural_main, shell::shell_main,
-    tomo::t_main,
-};
 
 #[tokio::main]
 async fn main() {
-    let auth = conf_init().unwrap();
-    println!("booting...");
+    // import config/reywen.json
+    let file = String::from_utf8(
+        std::fs::read("config/reywen.json").expect("unable to find file config/reywen.json"),
+    )
+    .expect("Failed to interpret byte array");
 
-    let ws = ws_init().unwrap();
+    // Deserilize reywen.json Websocket and Auth
+    let websocket =
+        serde_json::from_str::<Websocket>(&file).expect("invalid json for websocket config");
 
-    println!("websocket established");
+    let auth = serde_json::from_str::<Auth>(&file).expect("invalid json for Auth config");
 
-    ws.clone()
-        .generate()
-        .await
-        .for_each(|message_in| async {
-            let message = from_ws(message_in);
-
-            let input_message = rev_message_in(message);
-
-            let input_message = match input_message {
-                Err(_) => return,
-                Ok(a) => a,
-            };
-            let client = Reywen::new(auth.clone(), &input_message);
-
-            tokio::join!(
-                br_main(&client, &input_message),
-                e6_main(&client, &input_message),
-                message_main(&client, &input_message),
-                plural_main(&client, &input_message),
-                t_main(&client, &input_message),
-                shell_main(&client, &input_message),
-            );
-        })
-        .await;
-}
-
-// Simple markdown formating
-pub fn md_fmt(message: &str, emoji: RE) -> String {
-    let emoji = RE::e(emoji);
-
-    format!("{emoji} $\\color{{grey}}\\small\\textsf{{{message}}}$")
-}
-
-// Custom emojis
-pub enum RE {
-    Search,
-    Send,
-    Rm,
-    Json,
-    Insert,
-}
-
-// Serilizing emojis
-impl RE {
-    pub fn e(self) -> String {
-        match self {
-            RE::Search => ":01GQE862YPERANAJC30GNKH625:",
-            RE::Send => ":01GQE848SKP794SKZYY8RTCXF1:",
-            RE::Rm => ":01GQE86CT9MKAHPTG55HMTG7TR:",
-            RE::Json => ":01GQE86K0CG3FWA0D6FRY7JT0R:",
-            RE::Insert => ":01GQE86SAYFDXZE2F39YHJMB1F:",
-        }
-        .to_string()
+    // restart websocekt - always
+    loop {
+        websocket
+            .clone()
+            .generate()
+            .await
+            // from websocket config establish a connection
+            .for_each(|message| async {
+                // for every message
+                if let Ok(raw_message) = message {
+                    // if the message is valid
+                    if let Ok(input_message) =
+                        serde_json::from_str::<Message>(&raw_message.into_text().unwrap())
+                    // and of type Message
+                    {
+                        let client = Do::new(&auth, &input_message);
+                        // spawn a new task
+                        tokio::spawn(message_process(client));
+                    }
+                }
+            })
+            .await;
     }
+}
+
+async fn message_process(client: Do) {
+    tokio::join!(
+        plugins::bridge::br_main(&client),
+        /*
+            plugins::e6::e6_main(&client),
+            plugins::message::message_main(&client),
+            plugins::plural::plural_main(&client),
+            plugins::tomo::t_main(&client),
+            plugins::shell::shell_main(&client),
+        */
+    );
+}
+
+// basic CLI tool for checking content
+pub fn crash_condition(input_message: &Message, character: Option<&str>) -> bool {
+    if input_message.content.is_none() {
+        return true;
+    };
+
+    let temp_convec: Vec<&str> = input_message
+        .content
+        .as_ref()
+        .unwrap()
+        .split(' ')
+        .collect::<Vec<&str>>();
+
+    let mut length = 2;
+
+    if character.is_none() {
+        length = 1;
+    };
+
+    if temp_convec.len() < length {
+        return true;
+    };
+
+    if character.is_some() && character != Some(temp_convec[0]) {
+        return true;
+    };
+    false
 }
