8fbba1a55064729ff66424358f8439899d67dcd1
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-30 15:30:59 +0800

    upgraded message.rs

--- src/plugins/message.rs
+++ src/plugins/message.rs
@@ -1,15 +1,7 @@
+use reywen::client::Do;
 use serde::{Deserialize, Serialize};
 
-use reywen::{
-    delta::{
-        fs::fs_to_str,
-        lreywen::{convec, crash_condition},
-        oop::Reywen,
-    },
-    quark::delta::message::RMessage,
-};
-
-use crate::{md_fmt, RE};
+use crate::{crash_condition, md_fmt, RE};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MessageConf {
@@ -17,7 +9,7 @@ pub struct MessageConf {
 }
 
 // main message engine
-pub async fn message_main(client: &Reywen, input_message: &RMessage) {
+pub async fn message_main(client: &Do) {
     let help = format!(
         "### Reywen-TXC\n{} {}\n{} {}\n{} {}\n{} {}\n{} {}\n{} {}",
         md_fmt("?e", RE::Send),
@@ -34,31 +26,32 @@ pub async fn message_main(client: &Reywen, input_message: &RMessage) {
         "Displays version"
     );
 
-    let client: Reywen = client.to_owned();
-
     // import config
-    let conf = fs_to_str("config/message.json").expect("failed to read config/message.json\n{e}");
 
-    let message_conf: MessageConf =
-        serde_json::from_str(&conf).expect("Failed to deser message.json");
+    let conf: MessageConf = serde_json::from_str(
+        &String::from_utf8(
+            std::fs::read("config/message.json").expect("failed to read config/message.json\n{e}"),
+        )
+        .unwrap(),
+    )
+    .expect("invalid config");
 
     // return if this plugin is disabled
-    if !message_conf.enabled {
+    if !conf.enabled {
         return;
     };
 
     // covers vector crash conditions
-    crash_condition(input_message, None);
+    crash_condition(&client.input_message, None);
 
-    // content vector
-    let convec = convec(input_message);
-
-    let mes = match convec[0] as &str {
+    // match the first word from websocket
+    let mes = match &client.convec()[0] as &str {
         "?Mog" | "?mog" => ":01G7MT5B978E360NB6VWAS9SJ6:",
         "?ver" | "?version" => "Reywen.rs 0.1.4",
         "?help" => "help",
         _ => "",
     };
+
     // if applicable, send
     if mes == "help" {
         client.sender(&help).await;
