f364736db255dea1306a7ed50c44939c6ed5cd2d
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-02-05 09:46:50 +0800

    updated main to 0.1.7

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,11 @@ use reywen::{
 // plugins
 pub mod plugins {
     pub mod bridge;
-    /*
-        pub mod e6;
-        pub mod message;
-        pub mod plural;
-        pub mod shell;
-        pub mod tomo;
-    */
+    pub mod e6;
+    pub mod message;
+    pub mod plural;
+    pub mod shell;
+    pub mod tomo;
 }
 
 #[tokio::main]
@@ -51,7 +49,7 @@ async fn main() {
                     {
                         let client = Do::new(&auth, &input_message);
                         // spawn a new task
-                        tokio::spawn(message_process(client));
+                        tokio::spawn(message_process(client.await));
                     }
                 }
             })
@@ -62,13 +60,11 @@ async fn main() {
 async fn message_process(client: Do) {
     tokio::join!(
         plugins::bridge::br_main(&client),
-        /*
-            plugins::e6::e6_main(&client),
-            plugins::message::message_main(&client),
-            plugins::plural::plural_main(&client),
-            plugins::tomo::t_main(&client),
-            plugins::shell::shell_main(&client),
-        */
+        plugins::e6::e6_main(&client),
+        plugins::message::message_main(&client),
+        plugins::plural::plural_main(&client),
+        plugins::tomo::t_main(&client),
+        plugins::shell::shell_main(&client),
     );
 }
 
@@ -99,4 +95,37 @@ pub fn crash_condition(input_message: &Message, character: Option<&str>) -> bool
         return true;
     };
     false
+}
+
+// Simple markdown formating
+pub fn md_fmt(message: &str, emoji: RE) -> String {
+    let emoji = RE::e(emoji);
+
+    format!("{emoji} $\\color{{grey}}\\small\\textsf{{{message}}}$")
+}
+
+// Custom emojis
+pub enum RE {
+    Search,
+    Send,
+    Rm,
+    Json,
+    Insert,
+}
+
+// Serilizing emojis
+impl RE {
+    pub fn e(self) -> String {
+        match self {
+            RE::Search => ":01GQE862YPERANAJC30GNKH625:",
+            RE::Send => ":01GQE848SKP794SKZYY8RTCXF1:",
+            RE::Rm => ":01GQE86CT9MKAHPTG55HMTG7TR:",
+            RE::Json => ":01GQE86K0CG3FWA0D6FRY7JT0R:",
+            RE::Insert => ":01GQE86SAYFDXZE2F39YHJMB1F:",
+        }
+        .to_string()
+    }
+}
+pub fn lte(input: &str) -> String {
+    format!("[]({input})")
 }
